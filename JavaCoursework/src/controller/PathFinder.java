68d4c3d358870b871ac5f22455d7a6d4bf6eea8f
ManiSC18 <manraj.chhokar@gmail.com>
2018-04-22 17:40:34 +0100

    Created PathFinder

---
+++ JavaCoursework/src/controller/PathFinder.java
@@ -0,0 +1,27 @@
+package controller;
+
+public class PathFinder {
+	
+	
+	
+	
+	public String getDirection(int currentX, int currentY, int destX, int destY) {
+		
+	int xDist = (destY - currentY);
+	int yDist = (destX - currentX);
+	
+	if (yDist == 0 && xDist == 0) {
+			return null;
+	}
+	
+	if (s)
+	
+		
+		
+		
+	}
+	
+	
+	
+
+}
