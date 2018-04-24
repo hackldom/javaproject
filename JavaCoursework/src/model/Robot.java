a30da2b86aef4f6433ea96336f23500d0a42c8a1
Dom Hackl <domhackl@gmail.com>
2018-04-24 17:41:44 +0100

    girls' version of Robot class

--- JavaCoursework/src/model/Robot.java
+++ JavaCoursework/src/model/Robot.java
@@ -9,22 +9,25 @@ public class Robot {
     private int battery;
     private static final int  MAX_BATTERY = 20;
     private Cell pos;
-    private boolean hasCrashed;
-    private boolean carrying;
+    private String item;
+    private boolean crashed;
+    private boolean gotItem;
     private ChargingPod pod;
     private String rID;
-    
+	private int charge;
+	private String cpID;
     static String NAME;
 	List<Robot>	robotList = new ArrayList<Robot>();
+
 	//public Cell[][] cell;
 
     
     public Robot(Cell cell, String rID, boolean isFree, int battery, ChargingPod pod){
-    pos = cell;
+    	pos = cell;
     	this.rID = rID;
     	battery = MAX_BATTERY;
-    	hasCrashed = false;
-    	carrying = false;
+    	crashed = false;
+    	gotItem = false;
     	this.pod = pod;
     }
 
@@ -43,90 +46,72 @@ public class Robot {
     	robotList.remove(r);
     }
     
-    public int getMaxBattery() {
-    	return MAX_BATTERY;
-    }
-    
-    public void move(int xChange, int yChange){
-    	if (!hasCrashed & !(returnToPod())){
-    		//move
-    		powerMinus();
-    		//checkLocation();
-    	}
-    }
-    /*public void checkLocation(){
-    	
-    	
-    	if (robotX == POD_X && robotY == POD_Y){
-    		charge();
-    	}
-    	/*if (hitRobot()){
-    		System.out.println("robot " + NAME + " crashed with another robot");
-    		//stop simulation
-    	}
-    	
-    	
-    	
-    
-   /*public boolean hitRobot(){
-    	for (int i = 0; grid.getRobots().length){
-    		if (!(grid.getRobots()[i].equals(grid.getRobots()[NAME]))){
-    			if (grid.getRobots()[i].getX() == robotX && grid.getRobots()[i].getY() == robotY){
-    				return true;
-    			}
-    		}
-    		i++;
-    	}
-    	return false;
-    }*/
-    
-    public Cell getCell(){
-    	return pos;
-    }
-    
-    
-    public int getX(){
-    	return pos.getX();
-    }
-    
-    public int getY(){
-    	return pos.getY();
-    }
-    
-    public ChargingPod getPod() {
-    	return pod;
-    }
-    
-    
-    private void powerMinus(){
-    	//use one if not carrying anything two if it is
-    	if(!hasCrashed) {
-    		if(!carrying) {
-    			battery--;
-    		}
-    		else if(carrying) {
-    			battery = battery -2;
-    		}
-    	}
-    	else if(hasCrashed) {
-    		System.out.println("One of the robots crashed, the simulation will now exit");
-    		//EXIT SIMULATION
-    	}
-    	
-    }
-    
-    public void acceptAssignment(){
-    	carrying = true;
-    }
-    public boolean returnToPod(){
-    	return false;
-    }
-    
-    public int getCharge() {
-    	return battery;
-    }
-    
-    public void charge() {
-    	battery++;
-    }
+	public void move(int xChange, int yChange){
+		if (!crashed){
+			//move
+			powerMinus();
+		}
+		else if (gotItem = true) {
+			powerMinus();
+			powerMinus();
+		}
+		else {
+			powerMinus();
+		}
+	}
+
+	public void pickItem(String itemName) {
+		item = itemName;
+		gotItem = true;
+	}
+
+	public void drop() {
+		item = "";
+		gotItem = false;
+	}
+
+	public int getX(){
+		return pos.getX();
+	}
+
+	public int getY(){
+		return pos.getY();
+	}
+
+	public boolean getCrashed() {
+		return crashed;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public boolean carrying() {
+		return gotItem;
+	}
+
+	public String getCPID() {
+		return cpID;
+	}
+
+	public String getRID() {
+		return rID;
+	}
+
+	public int getCharge() {
+		return charge;
+	}
+
+	public void charge() {
+		charge++;
+	}
+
+	private void powerMinus(){
+		//use one if not carrying anything two if it is
+		charge = charge - 1;
+		if (charge == 0){
+			crashed = true;
+		}
+	}
+
 }
